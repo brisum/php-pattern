66baada91c1787ca8a0dd353deda76934cffde8f
Oleksandr Manchenko <omanchenko@brisum.com>
2021-03-08 22:11:41 +0200

    Symfony DI

--- bootstrap.php
+++ bootstrap.php
@@ -2,4 +2,14 @@
 
 declare(strict_types=1);
 
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+
 require_once __DIR__ . '/vendor/autoload.php';
+
+$di = new ContainerBuilder();
+$loader = new YamlFileLoader($di, new FileLocator(__DIR__ . '/config/'));
+$loader->load('services.yaml');
+
+$di->compile();
