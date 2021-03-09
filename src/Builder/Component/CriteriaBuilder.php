2ef14bfa2729176da521738252b7d4e5cecf78ee
Oleksandr Manchenko <omanchenko@brisum.com>
2021-03-10 00:01:46 +0200

    Builder

---
+++ src/Builder/Component/CriteriaBuilder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Brisum\Pattern\Builder\Component;
+
+class CriteriaBuilder
+{
+    private array $criteria = [];
+
+    public function getCriteria(): Criteria
+    {
+        return new Criteria($this->criteria);
+    }
+
+    public function setCountry(string $country): self
+    {
+        $this->criteria['country'] = $country;
+
+        return $this;
+    }
+
+    public function setCurrency(string $currency): self
+    {
+        $this->criteria['currency'] = $currency;
+
+        return $this;
+    }
+}
